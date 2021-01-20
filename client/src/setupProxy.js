308e3365f39d5f2b90f10c0b3562bb347f1274da
Tony <unclestrong@Tonys-MacBook-Air.local>
2021-01-20 15:46:40 -0800

    changed proxy to see if that works

--- client/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-const {createProxyMiddleware} = require("http-proxy-middleware");
-
-module.exports = function(app) {
-  app.use(
-    createProxyMiddleware(["/api", , "/otherApi"], { target: "https://downforsomething.herokuapp.com:5000" })
-  );
-};
