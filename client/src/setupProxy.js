16659752318e4bd8bd161d83c40d2545010e5101
Tony <unclestrong@Tonys-MacBook-Air.local>
2021-01-20 16:11:07 -0800

    fixing error in last commit

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,6 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware(["/", , "/"], { target: "http://localhost:5000" })
+    createProxyMiddleware(["/api", , "/otherApi"], { target: "http://localhost:5000" })
   );
 };
