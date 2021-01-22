81041e9a632946a2b244a9892f2c5a78858c32f3
Tony <unclestrong@Tonys-MacBook-Air.local>
2021-01-22 11:56:56 -0800

    fixed possible typo

--- server/index.js
+++ server/index.js
@@ -2,38 +2,43 @@ const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const express = require('express');
+const app = express();
 
 require('dotenv').config();
 const PORT = process.env.PORT || process.env.DEV_PORT;
 const INDEX = '/index.html';
 
-const io = require('socket.io')
-const server = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  pingTimeout: 30000,
+  cors: {
+    origin: '*',
+  }
+});
 
 const cors = require('cors');
-const mysql = require('mysql');
+const mysql = require("mysql");
 const phase1Route = require('./routes/phase1');
 const phase2Route = require('./routes/phase2');
 const plansRoute = require('./routes/plans');
 const prismaFunc = require('./functions/prisma');
 
-server.use((req, res) => res.sendFile(INDEX, { root: __dirname }));
-server.use(express.json());
-server.use(express.urlencoded({extended: false}));
-server.use(cors());
-server.use('/api/phase1', phase1Route);
-server.use('/api/phase2', phase2Route);
-server.use('/api/plans', plansRoute);
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use('/api/phase1', phase1Route);
+app.use('/api/phase2', phase2Route);
+app.use('/api/plans', plansRoute);
 
 let connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection(process.env.DATABASE_URL);
 }
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use.use((req, res) => res.sendFile(INDEX, {root: __dirname}));
 }
 
