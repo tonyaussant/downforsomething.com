1dc6bcbf49f55c721a8efea64c026811335ec396
Tony <unclestrong@Tonys-MacBook-Air.local>
2021-01-25 11:38:18 -0800

    added environment variable

--- client/src/components/CreatePlan.jsx
+++ client/src/components/CreatePlan.jsx
@@ -3,7 +3,7 @@ import {Redirect} from 'react-router-dom';
 import {io} from 'socket.io-client';
 import randomize from 'randomatic';
 import Header from './children/elements/Header';
-const BACKEND_URL = process.env.NODE_ENV === 'development'
+const BACKEND_URL = process.env.NODE_ENV === 'production'
 ? 'https://downforsomething.herokuapp.com' : process.env.REACT_APP_BACKEND_URL;
 
 class CreatePlan extends Component {
