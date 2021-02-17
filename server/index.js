f27987764e4157e02c995291cf50dfb31f3a5770
Tony <unclestrong@Tonys-MacBook-Air.local>
2021-02-17 12:58:28 -0800

    fixed syntx error

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
-const PORT = env(PORT) || env(DEV_PORT);
+const PORT = process.env.PORT || process.env.DEV_PORT;
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
