ddfea7986ea5ae2a16b2301c946be966a3d8850d
TonyAussant <tonydaussant@gmail.com>
2021-10-20 10:30:10 -0700

    added yarn to project and upgraded packages

--- client/src/components/children/elements/Link.jsx
+++ client/src/components/children/elements/Link.jsx
@@ -2,7 +2,7 @@ function Link(props) {
   return(
     <li className='link'>
       <h3 className='text link__name'>{props.name}</h3>
-      <a className='text link__url' rel='noreferrer' target='_blank' href={props.url}>{props.url}</a>
+      <a className='text link__url' rel='noopener noreferrer' target='_blank' href={props.url}>{props.url}</a>
     </li>
   );
 }
